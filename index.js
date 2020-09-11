c84ab2e88b7d8dfed88b70c53afaebf4e1e0159a
LWRNC75 <lawrence.kartodimedjo@gmail.com>
2020-09-11 09:52:23 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const myColors = ["yellow", "Blue", "Red", "Orange"];
+// While loop
+// let i = 0;
+// while (i < myColors.length) {
+//     console.log(myColors[i]);
+//     i++;
+// }
+
+// For Loop
+// for (let i = 0; i < myColors.length; i++) {
+//     console.log(myColors[i]);
+// }
+
+
+// For each
+myColors.forEach((color) => console.log(color));
+
+// 1. Hoeveel regels neemt mijn for loop en mijn while loop in beslag?
+// De For Loop heeft 3 regels t.o.v. While loop
+// 2. Hoeveel regels neemt mijn forEach method in beslag?
+// For each heeft 1 regel.
+// 3. Welke voordelen heeft het gebruiken van een array method nog meer t.o.v. een for of een while loop? Welke methode vind je bijvoorbeeld makkelijker leesbaar? Waarom?
+// Het is compacter je gebrukt geen iterate meer en persoonlijker vind ik for each fijner om te schrijven.
+
